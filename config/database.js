6399218bc69033ef488a882cec536d6bfdae65dd
Rohan Wayachal <ronwayachal@gmail.com>
2018-11-18 12:36:33 -0800

    commit dbfix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-
+   database:'mongodb://bazaar:madskull007@ds211558.mlab.com:11558/bazaar',
  // database: 'mongodb://localhost:27017/bazaar',    //dev
   secret: 'yoursecret'
 }
